41bd570e78a1815c1c1a23575160fd9d92461c8f
orbu13 <orelbutbul13@gmail.com>
2023-06-14 11:18:35 -0700

    school task

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+// business logic //
+
+function getRsndomIntInclusive(min, max){
+    min = Math.ceil(1);
+    max = Math.ceil(6);
+    return Math.floor(Math.random() * (max - min + 1) + min); // the maximum is inclusive and the minimum is inclusive
+}
+function randomIntegerReturn
+const randomNumber = getRsndomIntInclusive = getRsndomIntInclusive();
+return randomNumber;
