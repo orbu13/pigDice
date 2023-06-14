674dc21320ca86d0a045208afaede186769a51a6
orbu13 <orelbutbul13@gmail.com>
2023-06-14 16:29:54 -0700

    fianlPush for today

--- js/test.js
+++ js/test.js
@@ -4,6 +4,7 @@ let playerOneTotal = 0
 let playerTwoTotal = 0
 let playerTurn = 1
 
+
 function getRsndomIntInclusive(min, max){
     min = Math.ceil(1);
     max = Math.ceil(6);
@@ -12,44 +13,46 @@ function getRsndomIntInclusive(min, max){
 
 function randomIntegerReturn() {
 let randomNumber = getRsndomIntInclusive();
-if (randomNumber !== 1){
-
+if (randomNumber !== 1) {
 rollTotal += randomNumber;
-
 return rollTotal;
 } else {
     rollTotal = 0
     return "You rolled a One"
 };
 }
+function checkFinalScore() {
+    if (playerOneTotal >= 5) {
+        playerTwoTotal = 0;
+        playerOneTotal = 0;
+        return "Player One Wins!";
+    }
+    else if (playerTwoTotal >= 5) {
+        playerOneTotal = 0;
+        playerTwoTotal = 0;
+        return "Player Two is the WINER!";
+    }
+}
 
 function pushButton() {
-    if (rollTotal >= 2 && playerTurn === 1) {
-      playerOneTotal += rollTotal;
-      rollTotal = 0;
-      playerTurn = 2;
-      console.log(playerTurn);
-      checkFinalScore ();
-    } else if (rollTotal >= 2 && playerTurn ===2 ) {
+        if (rollTotal >= 2 && playerTurn === 1) {
+          playerOneTotal += rollTotal;
+          rollTotal = 0;
+          playerTurn = 2;
+          console.log("Player " + playerTurn);
+          let checkFinal = checkFinalScore()
+          return checkFinal;
+        } else if (rollTotal >= 2 && playerTurn ===2 ) {
             playerTwoTotal += rollTotal;
             rollTotal = 0;  
             playerTurn = 1; 
-            console.log(playerTurn);
-            checkFinalScore ();
-    } else {
-        return "You have not rolled your dice yet!"
-    }
-}
-function checkFinalScore () {
-    if (playerOneTotal >= 20) {
-        return "Player One Wins!" ||
-        playerTwoTotal = 0;
-        playerOneTotal = 0;||
-    }
-    else if (playerTwoTotal >=20){
-        return "Player Two is the WINER!"
-        playerOneTotal = 0;
-        playerTwoTotal = 0;
-    }
-    
-}    
+            console.log("Player " + playerTurn);
+            let checkFinal = checkFinalScore()
+            return checkFinal;
+        } else {
+            return "You have not rolled your dice yet!"
+        }
+       
+}  
+
+ 
