0bf7aa9fb8537407d256b8106853e44889c5ee29
orbu13 <orelbutbul13@gmail.com>
2023-06-14 11:25:09 -0700

    trying to push

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,21 @@
 // business logic //
+let rollTotal = 0
 
 function getRsndomIntInclusive(min, max){
     min = Math.ceil(1);
     max = Math.ceil(6);
     return Math.floor(Math.random() * (max - min + 1) + min); // the maximum is inclusive and the minimum is inclusive
 }
-function randomIntegerReturn
-const randomNumber = getRsndomIntInclusive = getRsndomIntInclusive();
-return randomNumber;
+
+function randomIntegerReturn() {
+let randomNumber = getRsndomIntInclusive();
+if (randomNumber !== 1){
+
+rollTotal += randomNumber;
+
+return rollTotal;
+} else {
+    rollTotal = 0
+    return "You rolled a One"
+}
+}
