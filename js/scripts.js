f9e8314aecc3a404ffbf9854eb1b530ed37e324a
orbu13 <orelbutbul13@gmail.com>
2023-06-15 10:29:18 -0700

    updating

--- js/scripts.js
+++ js/scripts.js
@@ -1,41 +1,69 @@
 // business logic //
-let rollTotal = 0
-let playerOneTotal = 0
-
-function getRsndomIntInclusive(min, max){
-    min = Math.ceil(1);
-    max = Math.ceil(6);
-    return Math.floor(Math.random() * (max - min + 1) + min); // the maximum is inclusive and the minimum is inclusive
+function Gameplay() {
+    this.rollTotal = 0;
+    this.playerOneTotal = 0;
+    this.playerTwoTotal = 0;
+    this.playerTurn = 1;
 }
 
-function randomIntegerReturn() {
-let randomNumber = getRsndomIntInclusive();
-if (randomNumber !== 1){
 
-rollTotal += randomNumber;
+Gameplay.prototype.getRsndomIntInclusive = function(min, max){
+    min = Math.ceil(1);
+    max = Math.ceil(6);
+    let mathInt = Math.floor(Math.random() * (max - min + 1) + min); // the maximum is inclusive and the minimum is inclusive
+    console.log(mathInt);
+    return mathInt
+}
 
-return rollTotal;
+Gameplay.prototype.randomIntegerReturn = function() {
+let randomNumber = this.getRsndomIntInclusive();
+if (randomNumber !== 1) {
+    console.log(this.rollTotal);
+this.rollTotal += randomNumber;
+return this.rollTotal;
 } else {
-    rollTotal = 0
+    this.rollTotal = 0
     return "You rolled a One"
 };
 }
+Gameplay.prototype.checkFinalScore = function() {
+    if (this.playerOneTotal >= 5) {
+        this.playerTwoTotal = 0;
+        this.playerOneTotal = 0;
+        return "Player One Wins!";
+    }
+    else if (Gameplay.prototype.playerTwoTotal >= 5) {
+        this.playerOneTotal = 0;
+        this.playerTwoTotal = 0;
+        return "Player Two is the WINER!";
+    }
+}
 
-function pushButton() {
-    if (rollTotal >= 2) {
-      playerOneTotal += rollTotal;
-      rollTotal = 0;
-    } else {
-        return "You have not rolled your dice yet!"
-    }
+Gameplay.prototype.pushButton = function() {
+        if (this.rollTotal >= 2 && this.playerTurn === 1) {
+            this.layerOneTotal += this.rollTotal;
+            this.rollTotal = 0;
+            this.playerTurn = 2;
+          console.log("Player " + this.playerTurn);
+          let checkFinal = checkFinalScore()
+          return checkFinal;
+        } else if (this.rollTotal >= 2 && this.playerTurn ===2 ) {
+            this.playerTwoTotal += this.rollTotal;
+            this.rollTotal = 0;  
+            this.playerTurn = 1; 
+            console.log("Player " + this.playerTurn);
+            let checkFinal = checkFinalScore()
+            return checkFinal;
+        } else {
+            return "You have not rolled your dice yet!"
+        }
+       
+}  
+//test 
+
+//UI
+window.onload = function(){
+
 }
-if (rollTotal = 0) {
-    playerTwoTotal += rollTotal;
-}
-/*function pushTotal() {
-    if (rollTotal >= 2) {
-        playerOneTotal += rollTotal;
-        rollTotal = 0;
-    }
-}
-}
+let pigGamePlay = new Gameplay();
+ 
